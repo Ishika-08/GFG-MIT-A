6c637dc916c9b6252e8dfdd94eb07924f5216756
Mobi <76767061+Mobiance@users.noreply.github.com>
2024-01-29 19:41:19 +0530

    Completed data entry

--- src/Data/TeamData.jsx
+++ src/Data/TeamData.jsx
@@ -1,60 +1,84 @@
 export const TeamMembersArray = [
-  {
-    id: 1,
-    name: "Rohit Bansode",
-    designation: "Head at GFG-MITA",
-    photourl:
-      "/TeamMembers/Main/Rohit-Bansode.jpg",
-  },
-  {
-    id: 2,
-    name: "Yashwant P. Nidhonkar",
-    designation: "Vice - Chairperson ",
-    photourl:
-      "/TeamMembers/Main/Yashwant-Nidhonkar.jpeg",
-  },
-  {
-    id: 3,
-    name: "Kartik Hajbe",
-    designation: "Social Media Team Lead",
-    photourl:
-      "/TeamMembers/Social-media/Kartik-Hajbe.jpg",
-  },
-  {
-    id: 4,
-    name: "Ishika Ranjan",
-    designation: "Technical Team Lead",
-    photourl:
-      "/TeamMembers/Tech/Ishika-Ranjan.jpeg",
-  },
-  {
-    id: 5,
-    name: "Nilesh Mankape",
-    designation: "Design and Content Team Lead",
-    photourl:
-    "https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80",
-  },
-  {
-    id: 6,
-    name: "Lisha Mehta",
-    designation: "PR & Outreach Team Lead",
-    photourl:
-      "/TeamMembers/PR/Lisha-Mehta.jpeg",
-  },
-  {
-    id: 7,
-    name: "Komal Sharma",
-    designation: "Event Team Lead",
-    photourl:
-      "/TeamMembers/Event/Komal-Sharma.jpeg",
-  },
-  {
-    id: 8,
-    name: "Rutuja Tamboli",
-    designation: "Marketing Team Lead",
-    photourl:
-      "/TeamMembers/Marketing/Rutuja-Tamboli.jpeg",
-  },
+    {
+        id: 1,
+        name: "Rohit Bansode",
+        designation: "Head at GFG-MITA",
+        photourl:
+            "/TeamMembers/Main/Rohit-Bansode.jpg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
+        id: 2,
+        name: "Yashwant P. Nidhonkar",
+        designation: "Vice - Chairperson ",
+        photourl:
+            "/TeamMembers/Main/Yashwant-Nidhonkar.jpeg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
+        id: 3,
+        name: "Kartik Hajbe",
+        designation: "Social Media Team Lead",
+        photourl:
+            "/TeamMembers/Social-media/Kartik-Hajbe.jpg",
+        linkedin: "https://in.linkedin.com/in/kartik-hajbe-538a99244",
+        twitter: "https://twitter.com/kartikhajbe",
+        instagram: "https://www.instagram.com/kartikhajbe/",
+    },
+    {
+        id: 4,
+        name: "Ishika Ranjan",
+        designation: "Technical Team Lead",
+        photourl:
+            "/TeamMembers/Tech/Ishika-Ranjan.jpeg",
+        linkedin: "https://www.linkedin.com/in/ishika-ranjan-75970a23b/",
+        twitter: "",
+        instagram: "https://www.instagram.com/ishika.ranjan.10/",
+    },
+    {
+        id: 5,
+        name: "Nilesh Mankape",
+        designation: "Design and Content Team Lead",
+        photourl:
+            "https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
+        id: 6,
+        name: "Lisha Mehta",
+        designation: "PR & Outreach Team Lead",
+        photourl:
+            "/TeamMembers/PR/Lisha-Mehta.jpeg",
+        linkedin: "https://www.linkedin.com/in/lisha-mehta-30729125b",
+        twitter: "",
+        instagram: "https://www.instagram.com/mehta_lisha",
+    },
+    {
+        id: 7,
+        name: "Komal Sharma",
+        designation: "Event Team Lead",
+        photourl:
+            "/TeamMembers/Event/Komal-Sharma.jpeg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
+        id: 8,
+        name: "Rutuja Tamboli",
+        designation: "Marketing Team Lead",
+        photourl:
+            "/TeamMembers/Marketing/Rutuja-Tamboli.jpeg",
+        linkedin: "https://www.linkedin.com/in/rutuja-tamboli-92a424258",
+        twitter: "",
+        instagram: "https://www.instagram.com/rutu_tamboli_?igsh=MWd5dDg2aHgzcnh3bw== https://www.linkedin.com/in/rutuja-tamboli-92a424258",
+    },
 ]
 
 
@@ -64,171 +88,237 @@ export const MainArray = [
         name: "Rohit Bansode",
         designation: "Head at GFG-MITA",
         photourl:
-          "/TeamMembers/Main/Rohit-Bansode.jpg",
-      },
-      {
+            "/TeamMembers/Main/Rohit-Bansode.jpg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
         id: 2,
         name: "Yashwant P. Nidhonkar",
         designation: "Vice - Chairperson ",
         photourl:
-          "/TeamMembers/Main/Yashwant-Nidhonkar.jpeg",
-      },
-  ]
+            "/TeamMembers/Main/Yashwant-Nidhonkar.jpeg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+]
 
-  export const TechArray = [
+export const TechArray = [
     {
         id: 1,
         name: "Ishika Ranjan",
         designation: "Technical Team Lead",
         photourl:
-          "/TeamMembers/Tech/Ishika-Ranjan.jpeg",
-      },
-      {
+            "/TeamMembers/Tech/Ishika-Ranjan.jpeg",
+        linkedin: "https://www.linkedin.com/in/ishika-ranjan-75970a23b/",
+        twitter: "",
+        instagram: "https://www.instagram.com/ishika.ranjan.10/",
+    },
+    {
         id: 2,
         name: "Yograj Hukumdar ",
         designation: "Technical Team Member",
         photourl:
-          "/TeamMembers/Tech/Yograj-Hukumdar.jpg",
-      },
-      {
+            "/TeamMembers/Tech/Yograj-Hukumdar.jpg",
+        linkedin: "https://www.linkedin.com/in/rana-yograj-b90111228",
+        twitter: "https://twitter.com/Rana__Yograj",
+        instagram: "",
+    },
+    {
         id: 3,
         name: "Shubham Sharma",
         designation: "Technical Team Member",
         photourl:
-          "/TeamMembers/Tech/Shubham-Sharma.jpg",
-      },
-      {
+            "/TeamMembers/Tech/Shubham-Sharma.jpg",
+        linkedin: "https://www.linkedin.com/in/shubham-sharma-6892871a1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "https://x.com/CallMeMobie?t=SIHZhdDMzKkoSX2naSLvDg&s=09",
+        instagram: "https://www.instagram.com/mobiance?igsh=MXYxNTNxcWN5c2k0ZQ==",
+    },
+    {
         id: 4,
         name: "Siyal Dhande",
         designation: "Technical Team Member",
         photourl:
-          "/TeamMembers/Tech/siyal.jpg",
-      },
-      {
+            "/TeamMembers/Tech/siyal.jpg",
+        linkedin: "https://www.linkedin.com/in/siyal-dhande02",
+        twitter: "https://twitter.com/Siyal02",
+        instagram: "https://www.instagram.com/siyal_dhande8",
+    },
+    {
         id: 5,
         name: "Shaikh Adnan",
         designation: "Technical Team Member",
         photourl:
-          "/TeamMembers/Tech/Adnan-Shaikh.jpg",
-      },
-  ]
+            "/TeamMembers/Tech/Adnan-Shaikh.jpg",
+        linkedin: "https://www.linkedin.com/in/sha1kh4",
+        twitter: "",
+        instagram: "",
+    },
+]
 
-  export const EventArray = [
+export const EventArray = [
     {
         id: 1,
         name: "Komal Sharma",
         designation: "Event Team Lead",
         photourl:
-          "/TeamMembers/Event/Komal-Sharma.jpeg",
-      },
-      {
+            "/TeamMembers/Event/Komal-Sharma.jpeg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
         id: 2,
         name: " Sukhada Deshpande",
         designation: "Event Team Member",
         photourl:
-          "/TeamMembers/Event/Sukhada-Deshpande.jpg",
-      },
-      {
+            "/TeamMembers/Event/Sukhada-Deshpande.jpg",
+        linkedin: "https://www.linkedin.com/in/sukhada-deshpande-79108325b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "https://x.com/SukhadaD1234?t=kbk2837A8-zp4Wo5_6j7gw&s=09",
+        instagram: "https://www.instagram.com/sukhada_deshpande24?igsh=bXA3NzExNDFieDNt",
+    },
+    {
         id: 3,
         name: "Chaitanya khadke ",
         designation: "Event Team Member",
         photourl:
-          "/TeamMembers/Event/Chaitanya-Khadke.jpg",
-      },
-      {
+            "/TeamMembers/Event/Chaitanya-Khadke.jpg",
+        linkedin: "https://www.linkedin.com/in/chaitanya-khadke-3992b326b",
+        twitter: "",
+        instagram: "",
+    },
+    {
         id: 4,
         name: "Apurva Sukale ",
         designation: "Event Team Member",
         photourl:
-          "/TeamMembers/Event/Apurva-Sukale.jpeg",
-      },
-  ]
+            "/TeamMembers/Event/Apurva-Sukale.jpeg",
+        linkedin: "https://www.linkedin.com/in/apurva-sukale-871902262?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "",
+        instagram: "https://www.instagram.com/sukaleapurva?igsh=b2JhYmwzMGo3bzVn",
+    },
+]
 
-  export const PRArray = [
+export const PRArray = [
     {
         id: 1,
         name: "Lisha Mehta",
         designation: "PR & Outreach Team Lead",
         photourl:
-          "/TeamMembers/PR/Lisha-Mehta.jpeg",
-      },
-      {
+            "/TeamMembers/PR/Lisha-Mehta.jpeg",
+        linkedin: "https://www.linkedin.com/in/lisha-mehta-30729125b",
+        twitter: "",
+        instagram: "https://www.instagram.com/mehta_lisha",
+    },
+    {
         id: 2,
         name: "Chitra Kakad",
         designation: "PR & Outreach Team Member",
         photourl:
-          "/TeamMembers/PR/Chitra-Kakad.jpg",
-      },
-  ]
+            "/TeamMembers/PR/Chitra-Kakad.jpg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+]
 
-  export const SocialMediaArray = [
+export const SocialMediaArray = [
     {
         id: 1,
         name: "Kartik Hajbe",
         designation: "Social Media Team Lead",
         photourl:
-          "/TeamMembers/Social-media/Kartik-Hajbe.jpg",
-      },
-      {
+            "/TeamMembers/Social-media/Kartik-Hajbe.jpg",
+        linkedin: "https://in.linkedin.com/in/kartik-hajbe-538a99244",
+        twitter: "https://twitter.com/kartikhajbe",
+        instagram: "https://www.instagram.com/kartikhajbe/",
+    },
+    {
         id: 2,
         name: "Monal Anerao",
         designation: "Social Media Team Member",
         photourl:
-          "/TeamMembers/Social-media/Monal-Anerao.jpg",
-      },
-      {
+            "/TeamMembers/Social-media/Monal-Anerao.jpg",
+        linkedin: "https://www.linkedin.com/in/monal-anerao",
+        twitter: "",
+        instagram: "https://www.instagram.com/monal.anerao?igsh=MnR3M3I3ZXJ4eXd1",
+    },
+    {
         id: 3,
         name: "Chaitanya Paraye",
         designation: "Social Media Team Member",
         photourl:
-          "/TeamMembers/Social-media/Chaitanya-Paraye.jpg",
-      },
-  ]
+            "/TeamMembers/Social-media/Chaitanya-Paraye.jpg",
+        linkedin: "https://www.linkedin.com/in/chaitanya-paraye-b79340260/",
+        twitter: "https://x.com/im_chaitanya_07?t=QZS47abSgLD1wr9y7faiDA&s=09",
+        instagram: "https://www.instagram.com/chaitanya_643_/",
+    },
+]
 
-  export const MarketingArray = [
+export const MarketingArray = [
     {
         id: 1,
         name: "Rutuja Tamboli",
         designation: "Marketing Team Lead",
         photourl:
-          "/TeamMembers/Marketing/Rutuja-Tamboli.jpeg",
-      },
-      {
+            "/TeamMembers/Marketing/Rutuja-Tamboli.jpeg",
+        linkedin: "https://www.linkedin.com/in/rutuja-tamboli-92a424258",
+        twitter: "",
+        instagram: "https://www.instagram.com/rutu_tamboli_?igsh=MWd5dDg2aHgzcnh3bw== https://www.linkedin.com/in/rutuja-tamboli-92a424258",
+    },
+    {
         id: 2,
         name: "Sanika Bhale",
         designation: "Marketing Team Member",
         photourl:
-          "/TeamMembers/Marketing/Sanika-Bhale.png",
-      },
-      {
+            "/TeamMembers/Marketing/Sanika-Bhale.png",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
         id: 3,
         name: "Meet Saraf",
         designation: "Marketing Team Member",
         photourl:
-          "/TeamMembers/Marketing/Meet-Saraf.jpg",
-      },
-  ]
+            "/TeamMembers/Marketing/Meet-Saraf.jpg",
+        linkedin: "https://www.linkedin.com/in/meet-saraf-992777244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "https://x.com/saraf_meet?t=rwv-IwhnJ1N0YlLX5e79Eg&s=09",
+        instagram: "https://www.instagram.com/sarafmeet?igsh=NWNwYXVsaHVmaWht",
+    },
+]
 
-  export const DesignArray = [
+export const DesignArray = [
     {
         id: 1,
         name: "Nilesh Mankape",
         designation: "Design and Content Team Lead",
         photourl:
-        "https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80",
-      },
-      {
+            "https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+    {
         id: 3,
         name: "Prasad Dinnapurkar",
         designation: "Design and Content Team Member",
         photourl:
-          "/TeamMembers/Design/Prasad-Dinnapurkar.jpeg",
-      },
-      {
+            "/TeamMembers/Design/Prasad-Dinnapurkar.jpeg",
+        linkedin: "https://www.linkedin.com/in/prasad-dinnapurkar-236256259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        twitter: "https://x.com/PDinnapurkar?t=ZcGbcKB0E9T8rLBpuBd6GA&s=08",
+        instagram: "https://www.instagram.com/prasaddinnapurkar_007/",
+    },
+    {
         id: 4,
         name: "Pratapsingh Misal ",
         designation: "Design and Content Team Member",
         photourl:
-          "/TeamMembers/Design/Pratap.jpg",
-      },
-  ]
+            "/TeamMembers/Design/Pratap.jpg",
+        linkedin: "",
+        twitter: "",
+        instagram: "",
+    },
+]
