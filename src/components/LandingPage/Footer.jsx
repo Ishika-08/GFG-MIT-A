b0e0cac66339b5a6346d17cc5ebe14d6a24b3e2b
Ishika-08 <ishikaranjan8@gmail.com>
2024-02-14 23:01:21 +0530

    changed linkedin url

--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -127,7 +127,7 @@ const Footer = () => {
           >
             <FaDiscord />
           </a>
-          <a href="https://www.linkedin.com/in/gfg-mit-student-chapter/" target="_blank" className="mr-4 text-xl neon-border">
+          <a href="https://www.linkedin.com/in/gfg-mita/" target="_blank" className="mr-4 text-xl neon-border">
             <FaLinkedinIn />
           </a>
         </div>
